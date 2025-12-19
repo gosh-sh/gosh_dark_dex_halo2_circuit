046396c3d94e65da74c1380391ed7ecd7518d547
alinaT <alina.t@gosh.sh>
2025-12-19 03:10:06 +0300

    serialize and deserialize try

--- src/test.rs
+++ src/test.rs
@@ -14,6 +14,7 @@ use halo2_base::halo2_proofs::{
     halo2curves::{bn256::Fr, secp256k1::{Fp, Fq, Secp256k1Affine}},
     plonk::Fixed,
 };
+use halo2_proofs::plonk::VerifyingKey;
 use halo2_proofs::{
     halo2curves::bn256::{Bn256, G1Affine},
     poly::{
@@ -27,9 +28,14 @@ use halo2_proofs::{
     plonk::{create_proof, keygen_pk, keygen_vk, verify_proof, Error},
     transcript::{TranscriptReadBuffer, TranscriptWriterBuffer, Blake2bRead, Blake2bWrite, Challenge255},
 };
-
+use halo2_proofs::SerdeFormat;
 use rand::random;
-
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Cursor, Read, Write},
+    path::Path,
+    rc::Rc,
+};
 #[test]
 fn simple_test() {
     let sk = random::<u64>();
@@ -61,23 +67,64 @@ fn full_test() {
     let private_note_sum_pub =  1000u64;
     let k = 18;
     let params: ParamsKZG<Bn256> = setup(k);
-    let proof = generate_proof(&params, Some(token_type), Some(private_note_sum), Some(sk), Some(pk), Some(g), token_type_pub, private_note_sum_pub);
+
+    let mut params_buf: Vec<u8> = Vec::new();
+
+    let _ = params.write_custom(&mut params_buf, SerdeFormat::RawBytesUnchecked).unwrap();
+    println!("KZG params len = {:?}", params_buf.len());
+
+    let mut params_slice: &[u8] = &params_buf;
+
+    let params_new  = ParamsKZG::<Bn256>::read_custom(&mut params_slice, SerdeFormat::RawBytesUnchecked).expect("Reading vkey should not fail");
+
+    
+
+
+    let proof = generate_proof(&params_new, Some(token_type), Some(private_note_sum), Some(sk), Some(pk), Some(g), token_type_pub, private_note_sum_pub);
     println!("proof len = {:?}", proof.len());
 
     let empty_circuit: DarkDexCircuit<Fr> = DarkDexCircuit::<Fr>::default();
-    let vk_from_empty = keygen_vk(&params, &empty_circuit).expect("keygen_vk should not fail");
+    let vk_from_empty = keygen_vk(&params_new, &empty_circuit).expect("keygen_vk should not fail");
 
-    let strategy = SingleStrategy::new(&params);
+    let mut vk1_buf: Vec<u8> = Vec::new();
+    vk_from_empty.write(&mut vk1_buf, SerdeFormat::RawBytesUnchecked)
+    .unwrap();
+    let mut slice: &[u8] = &vk1_buf;
+    println!("vk1_buf len = {:?}", vk1_buf.len());
+
+    let vk_from_empty_new: VerifyingKey<G1Affine> = VerifyingKey::read::<_, DarkDexCircuit<Fr>>(&mut slice, SerdeFormat::RawBytesUnchecked).expect("Reading vkey should not fail");
+
+    //let vk_from_empty_from_bytes: VerifyingKey<G1Affine> = VerifyingKey::< DarkDexCircuit<Fr>>::from_bytes(&vk1_buf, SerdeFormat::RawBytesUnchecked).unwrap();
+
+    //let vk: VerifyingKey<G1Affine> = VerifyingKey::read::<_, DarkDexCircuit<Fr>>(&mut vk1_buf, SerdeFormat::RawBytesUnchecked)
+                //.expect("Reading vkey should not fail");
+
+    //let path = format!("./data/1.vkey");
+
+    /*match File::open(path.as_str()) {
+        Ok(f) => {
+            let mut bufreader = BufReader::new(f);
+            let vk: VerifyingKey<G1Affine> = VerifyingKey::read::<_, DarkDexCircuit<Fr>>(&mut slice, SerdeFormat::RawBytesUnchecked)
+                .expect("Reading vkey should not fail");
+        
+        }
+        Err(_) => {
+        }
+    }*/
+
+    let strategy = SingleStrategy::new(&params_new);
     let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
 
-    verify_proof::<KZGCommitmentScheme<Bn256>, VerifierSHPLONK<_>, _, _, _>(
-        &params,
-        &vk_from_empty,
-        strategy,
-        &[&[&[Fr::from(1u64), Fr::from(1000u64)]]],
-        //&[&[]],
-        &mut transcript,
-    )
-    .unwrap();
+    assert!(
+        verify_proof::<KZGCommitmentScheme<Bn256>, VerifierSHPLONK<_>, _, _, _>(
+            &params_new,
+            &vk_from_empty_new,
+            strategy,
+            &[&[&[Fr::from(1u64), Fr::from(1000u64)]]],
+            //&[&[]],
+            &mut transcript,
+        )
+        .is_ok()
+    );
 
 }
