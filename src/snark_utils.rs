1cb0003b5a0a07fadf780b1ce966e6f74f441390
alinaT <alina.t@gosh.sh>
2026-01-28 21:11:49 +0300

    remove output

--- src/snark_utils.rs
+++ src/snark_utils.rs
@@ -43,8 +43,8 @@ pub fn setup_and_backup_kzg_params(k: u32, path: String) {
 pub fn read_kzg_params(path: String) -> ParamsKZG<Bn256>{
     let mut params_buf: Vec<u8> = std::fs::read(path).unwrap();
     let mut params_slice: &[u8] = &params_buf;
-    println!("KZG params_slice = {:?}", params_slice);
-    println!("KZG params len = {:?}", params_slice.len());
+    //println!("KZG params_slice = {:?}", params_slice);
+    //println!("KZG params len = {:?}", params_slice.len());
     let params  = ParamsKZG::<Bn256>::read_custom(&mut params_slice, SerdeFormat::RawBytesUnchecked).expect("Reading vkey should not fail");
     params
 }
@@ -91,8 +91,8 @@ pub fn generate_verififcation_key_without_witness_and_backup<C: Circuit<Fr> + De
     let mut vk1_buf: Vec<u8> = Vec::new();
     vk_from_empty.write(&mut vk1_buf, SerdeFormat::RawBytesUnchecked)
     .unwrap();
-    println!("vk1_buf len = {:?}", vk1_buf.len());
+    //println!("vk1_buf len = {:?}", vk1_buf.len());
   
-    println!("vk1 = {:?}", hex::encode(vk1_buf.clone()));
+    //println!("vk1 = {:?}", hex::encode(vk1_buf.clone()));
     std::fs::write(path, vk1_buf).unwrap();
 }
